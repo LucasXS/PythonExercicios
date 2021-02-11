d868eb0f28c5702391ee411b695d83379257c57c
lucas <contato_lucassantos@outlook.com>
2021-02-11 18:28:18 -0300

    pygame (tocando mp3)

--- exer021.py
+++ exer021.py
@@ -1,4 +1,9 @@
 #Abra e reproduza o áudio de um arquivo MP3
 
-import pyglet
+import pygame
 
+pygame.mixer.init() #inicia
+pygame.mixer.music.load('exer021.mp3') #carrega a music
+pygame.mixer.music.play() #toca a musica
+
+input('Agora é só relaxar')
