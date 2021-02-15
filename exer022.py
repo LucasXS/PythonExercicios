5492fae13b4f5647aed2dba5cb58a2ad3b725030
lucas <contato_lucassantos@outlook.com>
2021-02-15 10:49:31 -0300

    Create exer022.py

---
+++ exer022.py
@@ -0,0 +1,6 @@
+#ler o nome completo e mostrar. 1-nome com letras MAISCULAS 2-Qnts letras ao total 3-Qnts letras tem 1 nome
+
+nome = input('Informe o seu nome completo: ')
+print(nome.upper())
+print(len(nome))
+print(len('lucas'))
