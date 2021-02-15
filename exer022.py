f6629bf7ddc53922ccb5d86664fdb796a7a5a616
lucas <contato_lucassantos@outlook.com>
2021-02-15 13:53:36 -0300

    Corrigido
    Correção do prof. Guanabara

--- exer022.py
+++ exer022.py
@@ -1,7 +1,7 @@
 #ler o nome completo e mostrar. 1-nome com letras MAISCULAS 2-Qnts letras ao total 3-Qnts letras tem 1 nome
 
-nome = input('Informe o seu nome completo: ')
-print(nome.upper())
-print(len(nome))
-#print(nome[:])
-print(len('lucas'))
+nome = str(input('Informe o seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print('Seu nome em maisculas é {}'.format(nome.upper()))
+print('Seu nome em minusculas é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome)- nome.count(' '))) #remv os espaços branco entre as palv.
--- exer026.py
+++ exer026.py
@@ -1,7 +1,7 @@
 #ler uma frase #1-Quantas vezes aparela a letra 'A'.
 #2-Em que posição ela aparece a primeira vez #3 Em que posição ela aparece na última vez.
 
-frase = input('Digite sua frese: ').upper().strip()
+frase = str(input('Digite sua frese: ')).upper().strip()
 print(len(frase))
 print('Vezes que aparece a letra A na frases: {}'.format(frase.count('A')))
 print('A letra "A" aparece primeiro na posição: {}'.format(frase.find('A')))
