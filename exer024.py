ad2ce695219975a0908483b93136f0cc5893793b
lucas <contato_lucassantos@outlook.com>
2021-02-15 14:16:15 -0300

    Update exer024.py

--- exer024.py
+++ exer024.py
@@ -1,5 +1,5 @@
 #ler o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 
-cidade = input('Digite o nome da cidade: ')
-cidade = cidade.upper()
-print('SANTO' in cidade.split()[0])
+cidade = str(input('Digite o nome da cidade: ')).strip().upper()
+print(cidade[:5] == 'SANTO') #correção
+print('SANTO' in cidade.split()[0]) #como eu fiz
