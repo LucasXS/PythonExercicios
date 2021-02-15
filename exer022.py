0ce96eb80654d503d4f3d9e3efb80e4708d2f645
lucas <contato_lucassantos@outlook.com>
2021-02-15 14:02:02 -0300

    Update exer022.py

--- exer022.py
+++ exer022.py
@@ -5,3 +5,6 @@ print('Analisando seu nome...')
 print('Seu nome em maisculas é {}'.format(nome.upper()))
 print('Seu nome em minusculas é {}'.format(nome.lower()))
 print('Seu nome tem ao todo {} letras'.format(len(nome)- nome.count(' '))) #remv os espaços branco entre as palv.
+#print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
+separa = nome.split()
+print('O seu primeiro nome é {} e ele possue {} letras'.format(separa[0], len(separa[0])))
