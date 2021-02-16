eb6433992daa2e63897c8250aea2d191f9fbb075
lucas <contato_lucassantos@outlook.com>
2021-02-16 13:25:00 -0300

    calcula de passagem

---
+++ exer031.py
@@ -0,0 +1,13 @@
+#Perg. a distância de uma viagem em Km. Calc. preço da passagem, cobrando R$0.50 p/Km para viagens até 200Km e R$0.45
+# p/ mais longas.
+
+cidadeAtual = str(input('Digite a cidade de partida: ')).strip().upper()
+cidade = str(input('Digite a cidade destino: ')).upper().strip()
+distancia = float(input('Digite a distância total do trajeto: '))
+
+if distancia <= 200:
+    valor = distancia * 0.50
+    print('A passagem de {} para {} custará R${:.2f}'.format(cidadeAtual, cidade, valor))
+else:
+    valor = distancia * 0.45
+    print('A passagem de {} para {} custará R${:.2f}'.format(cidadeAtual, cidade, valor))
