8206306e2185701a2c0938090080b68189995750
lucas <contato_lucassantos@outlook.com>
2021-02-16 13:08:20 -0300

    multado por excesso velocidade

---
+++ exer029.py
@@ -0,0 +1,9 @@
+# ler a veloc. um carro. Se passar de 80Km/h, mostrre uma msg que ele foi multado. A multa R$7.00 p/Km acima do limite
+
+velocidade = float(input('Velocidade final: '))
+if velocidade > 80:
+    print('Voce estava acima da velocidade permitida de 80Km. Será multado por isso.')
+    multa = (velocidade - 80) * 7
+    print('Sua multa total é de R${:.2f}, com vencimento para daqui 7 dias úteis!'.format(multa))
+else:
+    print('Você estava na velocidade permitida')
